fd1ed42543932bd34dbbf084913a99be47f79c39
jyotishankar04 <patrajyotishankar@gmail.com>
2025-09-07 01:58:44 +0530

    setup llm

--- cal-personal-assistant/index.ts
+++ cal-personal-assistant/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import { ChatGroq } from "@langchain/groq";
+
+const GROQ_MODEL_NAME = process.env.GROQ_MODEL_NAME;
+const tools : any[] = []
+
+
+const model = new ChatGroq({
+    model: GROQ_MODEL_NAME!,
+    temperature: 0,
+}).bindTools(tools);
